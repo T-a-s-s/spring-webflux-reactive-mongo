c512a4973983a973808c0982b84ed38342ba4b44
Tomas Anderson Souza Silva <tomas.liveid@hotmail.com>
2018-07-17 00:01:22 -0300

    inicio

---
+++ src/main/java/br/com/neoholding/oi/garcom/OiGarcomApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.neoholding.oi.garcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OiGarcomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OiGarcomApiApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/neoholding/oi/garcom/OiGarcomApiApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.neoholding.oi.garcom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OiGarcomApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("Opa!!!");
+	}
+
+}
