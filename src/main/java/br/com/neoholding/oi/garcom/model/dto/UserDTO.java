6aca10409743ff6abbf3e11ae907b6caef580890
Tomas Anderson Souza Silva <tomas.liveid@hotmail.com>
2018-07-25 23:58:13 -0300

    métodos funcionando

--- src/main/java/br/com/neoholding/oi/garcom/model/dto/UserDTO.java
+++ src/main/java/br/com/neoholding/oi/garcom/model/dto/UserDTO.java
@@ -2,8 +2,10 @@ package br.com.neoholding.oi.garcom.model.dto;
 
 import br.com.neoholding.oi.garcom.enumeration.OiRole;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class UserDTO
 {
     String name;
