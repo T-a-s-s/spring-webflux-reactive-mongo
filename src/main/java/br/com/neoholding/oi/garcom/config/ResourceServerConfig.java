fcac14a6fa5696e7f143d4e648a19d4b0184c4db
Tomas Anderson Souza Silva <tomas.liveid@hotmail.com>
2018-07-26 16:10:29 -0300

    jwt

---
+++ src/main/java/br/com/neoholding/oi/garcom/config/ResourceServerConfig.java
@@ -0,0 +1,36 @@
+package br.com.neoholding.oi.garcom.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter
+{
+    private String RESOURCE_ID;
+
+    @Autowired
+    public ResourceServerConfig(@Value("${resource_id}") String RESOURCE_ID) {
+        this.RESOURCE_ID = RESOURCE_ID;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID).stateless(false);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.
+                anonymous().disable()
+                .authorizeRequests()
+                .antMatchers("/users/**").access("hasRole('ADMIN')")
+                .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+    }
+}
